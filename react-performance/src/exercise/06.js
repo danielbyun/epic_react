8e76f49e6a44dec9d1f8384e0db69d40dbd03ad8
danielbyun <dbyun92@gmail.com>
2021-01-08 21:18:33 +0900

    Finished first extra credit for Fix perf death by a thousand cuts

--- react-performance/src/exercise/06.js
+++ react-performance/src/exercise/06.js
@@ -12,6 +12,7 @@ import {
 
 const AppStateContext = React.createContext()
 const AppDispatchContext = React.createContext()
+const DogContext = React.createContext()
 
 const initialGrid = Array.from({length: 100}, () =>
   Array.from({length: 100}, () => Math.random() * 100),
@@ -19,11 +20,6 @@ const initialGrid = Array.from({length: 100}, () =>
 
 const appReducer = (state, action) => {
   switch (action.type) {
-    // we're no longer managing the dogName state in our reducer
-    // 💣 remove this case
-    // case 'TYPED_IN_DOG_INPUT': {
-    //   return {...state, dogName: action.dogName}
-    // }
     case 'UPDATE_GRID_CELL': {
       return {...state, grid: updateGridCellState(state.grid, action)}
     }
@@ -39,7 +35,7 @@ const appReducer = (state, action) => {
 const AppProvider = ({children}) => {
   const [state, dispatch] = React.useReducer(appReducer, {
     // 💣 remove the dogName state because we're no longer managing that
-    // dogName: '',
+    dogName: '',
     grid: initialGrid,
   })
   return (
@@ -63,6 +59,36 @@ const useAppDispatch = () => {
   const context = React.useContext(AppDispatchContext)
   if (!context) {
     throw new Error('useAppDispatch must be used within the AppProvider')
+  }
+  return context
+}
+
+const dogReducer = (state, action) => {
+  switch (action.type) {
+    // we're no longer managing the dogName state in our reducer
+    // 💣 remove this case
+    case 'TYPED_IN_DOG_INPUT': {
+      return {...state, dogName: action.dogName}
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`)
+    }
+  }
+}
+
+const DogProvier = props => {
+  const [state, dispatch] = React.useReducer(dogReducer, {
+    dogName: '',
+  })
+
+  const value = [state, dispatch]
+  return <DogContext.Provider value={value} {...props} />
+}
+
+const useDogState = () => {
+  const context = React.useContext(DogContext)
+  if (!context) {
+    throw new Error(`useDogState must be used within the DogStateProvder`)
   }
   return context
 }
@@ -108,22 +134,13 @@ Cell = React.memo(Cell)
 const DogNameInput = () => {
   // 🐨 replace the useAppState and useAppDispatch with a normal useState here
   // to manage the dogName locally within this component
-  const [state, setState] = React.useState({
-    dogName: '',
-  })
-  // const state = useAppState()
-  // const dispatch = useAppDispatch()
+  const [state, dispatch] = useDogState()
   const {dogName} = state
 
   const handleChange = event => {
     const newDogName = event.target.value
     // 🐨 change this to call your state setter that you get from useState
-    setState(() => {
-      return {
-        dogName: newDogName,
-      }
-    })
-    // dispatch({type: 'TYPED_IN_DOG_INPUT', dogName: newDogName})
+    dispatch({type: 'TYPED_IN_DOG_INPUT', dogName: newDogName})
   }
 
   return (
@@ -148,12 +165,14 @@ const App = () => {
   return (
     <div className="grid-app">
       <button onClick={forceRerender}>force rerender</button>
-      <AppProvider>
-        <div>
+      <div>
+        <DogProvier>
           <DogNameInput />
+        </DogProvier>
+        <AppProvider>
           <Grid />
-        </div>
-      </AppProvider>
+        </AppProvider>
+      </div>
     </div>
   )
 }
