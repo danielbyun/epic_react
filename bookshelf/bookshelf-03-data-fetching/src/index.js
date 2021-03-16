b13ba357a71aba1f574efc4e11ee9856154fab4f
danielbyun <dbyun92@gmail.com>
2021-03-16 22:12:18 +0900

    test

--- bookshelf/bookshelf-03-data-fetching/src/index.js
+++
@@ -1,9 +0,0 @@
-import {loadDevTools} from './dev-tools/load'
-import './bootstrap'
-import * as React from 'react'
-import ReactDOM from 'react-dom'
-import {DiscoverBooksScreen} from './discover'
-
-loadDevTools(() => {
-  ReactDOM.render(<DiscoverBooksScreen />, document.getElementById('root'))
-})
