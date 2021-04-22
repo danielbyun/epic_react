e63b2db8e501deb7f09c6f23a790c19b60be2038
danielbyun <dbyun92@gmail.com>
2021-04-22 09:50:54 +0900

    custom react profiler to monitor app, add metada and profile book screen

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -1,1 +1,47 @@
+import React from 'react'
+import {client} from 'utils/api-client'
+
+let queue = []
+
+setInterval(sendProfileQueue, 5000)
+
+function sendProfileQueue() {
+  if (!queue.length) {
+    return Promise.resolve({success: true})
+  }
+
+  const queueToSend = [...queue]
+  queue = []
+
+  return client('profile', {data: queueToSend})
+}
+
 // this is for extra credit
+const Profiler = ({phases, metadata, ...props}) => {
+  const reportProfile = (
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions,
+  ) => {
+    if (!phases || phases.includes(phase)) {
+      queue.push({
+        metadata,
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions,
+      })
+    }
+  }
+
+  return <React.Profiler onRender={reportProfile} {...props} />
+}
+
+export {Profiler}
