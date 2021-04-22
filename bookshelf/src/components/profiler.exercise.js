6f731afeb8e3e0945f052e709f9a37b30a5b1820
danielbyun <dbyun92@gmail.com>
2021-04-22 09:39:50 +0900

    add custom profiler to send to backend

--- bookshelf/src/components/profiler.exercise.js
+++ bookshelf/src/components/profiler.exercise.js
@@ -1,1 +1,46 @@
+import React from 'react'
+import {client} from 'utils/api-client'
+
+let queue = []
+
+setInterval(sendProfileQueue, 5000)
+
+const sendProfileQueue = () => {
+  if (!queue.length) {
+    return Promise.resolve({success: true})
+  }
+
+  const queueToSend = [...queue]
+  queue = []
+
+  return client('profile', {data: queueToSend})
+}
+
 // this is for extra credit
+const Profiler = ({phases, ...props}) => {
+  const reportProfile = (
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions,
+  ) => {
+    if (!phases || phases.includes(phase)) {
+      queue.push({
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions,
+      })
+    }
+  }
+
+  return <React.Profiler onRender={reportProfile} {...props} />
+}
+
+export {Profiler}
