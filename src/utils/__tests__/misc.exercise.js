0f486be3d567159588409dfb1805b3bc0165d63f
danielbyun <dbyun92@gmail.com>
2021-04-23 19:19:18 +0900

    start unit testing worshop

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,5 @@
-test.todo('formatDate formats the date to look nice')
+const {formatDate} = require('utils/misc')
 
+test('formatDate formats the date to look nice', () => {
+  expect(formatDate(new Date('October 18, 1988'))).toBe('Oct 88')
+})
