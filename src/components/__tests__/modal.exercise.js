121ba1e4357b4076abc8d510ffa89617bcb57f5a
danielbyun <dbyun92@gmail.com>
2021-04-27 19:36:00 +0900

    continue testing hooks and components

--- src/components/__tests__/modal.exercise.js
+++ src/components/__tests__/modal.exercise.js
@@ -1,20 +1,32 @@
 // 🐨 you're gonna need this stuff:
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, within} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {Modal, ModalContents, ModalOpenButton} from '../modal'
 
 test('can be opened and closed', () => {
+  const label = 'Modal Label'
+  const title = 'Modal Title'
   render(
     <Modal>
       <ModalOpenButton>
         <button>Open</button>
       </ModalOpenButton>
-      <ModalContents aria-label="Modal Label" title="Modal Title">
+      <ModalContents aria-label={label} title={title}>
         <div>Modal Content</div>
       </ModalContents>
     </Modal>,
   )
+  userEvent.click(screen.getByRole('button', {name: /open/i}))
+
+  const modal = screen.getByRole('dialog')
+  expect(modal).toHaveAttribute('aria-label', label)
+
+  const inModal = within(modal)
+  expect(inModal.getByRole('heading', {name: title})).toBeInTheDocument()
+
+  userEvent.click(inModal.getByRole('button', {name: /close/i}))
+  expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
 
   screen.debug()
 })
