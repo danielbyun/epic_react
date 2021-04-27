925ec1e332cb2ed73342b081992f05f190c00eba
danielbyun <dbyun92@gmail.com>
2021-04-27 19:36:19 +0900

    continue testing hooks and components

--- bookshelf/src/setupTests.exercise.js
+++ bookshelf/src/setupTests.exercise.js
@@ -1,6 +1,9 @@
-// this isn't used in the soultion. Only in the extra credit
+// 🐨 enable jest-dom by
+import '@testing-library/jest-dom'
 import {server} from 'test/server'
 
+// enable API mocking in test runs using the same request handlers
+// as for the client-side mocking.
 beforeAll(() => server.listen())
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
